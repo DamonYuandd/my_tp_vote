a41092cf032cca295418c1f011f1c23aecba6e89
damonyuan_nmg <damonyuan@nmg.com.hk>
2016-06-17 18:24:30 +0800

    添加ignore

--- App/Runtime/Cache/Home/a6f745875479b10b505c849069308b19.php
+++
@@ -1,3 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><div style="background:url(__HOME__/images/bottom_bg.jpg);">
-  <div id="bottom"><?php echo ($system["copyright"]); ?>  <?php echo ($system["icpnumber"]); ?></div>
-</div>
--- App/Runtime/Cache/Home/b3e4a541bb51c2f633808cd5b6c5664c.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><?php if(is_array($dataList)): $i = 0; $__LIST__ = $dataList;if( count($__LIST__)==0 ) : echo "没有相关信息" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><div class="pro_box">
-          <div class="pro_box_img"><a href="__APP__/<?php echo MODULE_NAME; ?>/show/cid/<?php echo ($vo["category_id"]); ?>/id/<?php echo ($vo["id"]); ?>.html" >
-          <img src="__PUBLIC__/images/product/m_<?php echo ($vo["image"]); ?>" width="500" height="376" /></a>
-          </div>
-          <div class="pro_box_d"><a href="__APP__/<?php echo MODULE_NAME; ?>/show/cid/<?php echo ($vo["category_id"]); ?>/id/<?php echo ($vo["id"]); ?>.html" ><?php echo (str_cut($vo["title"],0,20)); ?></a></div>
-        </div><?php endforeach; endif; else: echo "没有相关信息" ;endif; ?>
